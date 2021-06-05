193046cf650fa829c8d06c526c78c21504974f6b
bennyE31 <bennye3797@gmail.com>
2021-06-04 19:22:21 -0500

    Made an actual device implementation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "i2c.h"
 #include "mcpxxxx.h"
 
@@ -5,7 +6,12 @@ int main() {
     Mcpxxxx rick = Mcpxxxx(0, 0x54);
     Mcpxxxx morty = Mcpxxxx(1, 0x45);
 
-    morty.get_state(0);
-    rick.set_state(0, 4);
+    morty.begin();
+    rick.begin();
+
+    std::cout << rick.get_state(0) << "\n";
+    morty.set_state(8, 1);
+    std::cout << rick.get_state(8) << "\n";
+
     return 0;
 }
